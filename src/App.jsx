db87ec6e31954c449ba17c646e399194950d279e
Eric Marques <eric.marquesgomesmarques@gmail.com>
2023-08-08 11:42:16 -0300

    Adicionado confetes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Counts from './components/Counts.jsx'
-import Fire from './assets/fire.png'
 import Fierce from './components/UI/Fierce'
+
 
 function App() {
   return (
--- src/components/Counts.jsx
+++ src/components/Counts.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react"
+import React, { useState } from "react";
 import '../components/Counts.css'
+import Confetti from 'react-confetti'
 
 function Counts() {
     const [count, setCount] = useState(0);
+    const [showConfetti, setShowConfetti] = useState(false)
+
+    const handleClick = () => {
+        setCount(count + 1);
+        setShowConfetti(true);
+
+        setTimeout(() => {
+            setShowConfetti(false);
+        }, 3000);
+    }
+
     return (
         <div className="Buttonstyle">
-            <button type="button" onClick={() => setCount(count + 1)}>Tasks done today <span>{count}</span></button>
+            <button type="button" onClick={handleClick}>Tasks done today <span>{count}</span></button>
+            {showConfetti && <Confetti />}
         </div>
     )
 }
 
-export default Counts
+export default Counts;
